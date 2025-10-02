a6f796529b76a59455097a88a9f8731b21782743
Alla Nasypanas <alla.nasypana@gmail.com>
2025-10-02 21:52:19 +0300

    add point

--- homework-1-default/src/app/loading.tsx
+++ homework-1-default/src/app/loading.tsx
@@ -2,7 +2,7 @@ export default function Loading() {
 
     return (
         <div className="flex  flex-1 justify-center items-center h-full" >
-            <p className='font-medium text-xl text-teal-700'>Loading ..</p>
+            <p className='font-medium text-xl text-teal-700'>Loading ...</p>
         </div>
     )
 }
